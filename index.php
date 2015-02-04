b2956ce8b73887bdb1398c7ebf0615a45a838846
Ellert Finnbogi <ellertfinnbogi@gmail.com>
2015-02-04 15:31:09 +0000

    Smávægilegar breytingar

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
+//require("user_methods.php")
 
-echo '<meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>';
 
-echo 'Hérna ætla ég að setja smá innslag um síðuna';
+include("header.html");
+
 
 ?>
